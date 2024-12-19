62738313deff889eb289dd7fc4654df8994d566a
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-19 14:25:34 -0500

    Test the Seperate Config

--- Environments/Development/dev-backend.tf
+++ Environments/Development/dev-backend.tf
@@ -3,7 +3,6 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstatestoragecloud"
     container_name       = "tfstate"
-    key                  = "dev.tfstate"
+    key                  = "Dev.tfstate"
   }
 }
-##
