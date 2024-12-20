f64925c03a9f9f4a620f916b3dac96a0a97eb4bc
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-20 00:49:36 -0500

    Checking the Terraform File Change

--- providers.tf
+++ providers.tf
@@ -7,6 +7,14 @@ terraform {
     }
   }
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstatestoragecloud"
+    container_name       = "tfstate"
+    key                  = "$terraformBackendFile"
+  }
+}
 
 provider "azurerm" {
   features{}
