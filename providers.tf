0995fda4db8672311e2b2a888be28cd0e24c86b9
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-19 22:48:11 -0500

    Staging TFSTATE Update

--- providers.tf
+++ providers.tf
@@ -13,7 +13,7 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstatestoragecloud"
     container_name       = "tfstate"
-    key                  = "production.tfstate"
+    key                  = "staging.tfstate"
   }
 }
 
