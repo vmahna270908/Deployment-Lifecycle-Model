0352148dba117d7017ded6bb0e109256d1acd66c
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-20 00:31:29 -0500

    Changed the Parameters

--- providers.tf
+++ providers.tf
@@ -8,15 +8,6 @@ terraform {
   }
 }
 
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstatestoragecloud"
-    container_name       = "tfstate"
-    key                  = "development.tfstate"
-  }
-}
-
 provider "azurerm" {
   features{}
 }
