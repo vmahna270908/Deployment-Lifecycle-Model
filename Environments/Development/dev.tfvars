347112ad15e5242da9c59f6bec0cdf3128f56fe9
vmahna270908 <vaibhavmahna56@gmail.com>
2024-12-23 13:56:28 -0500

    Creating the vNet

--- Environments/Development/dev.tfvars
+++ Environments/Development/dev.tfvars
@@ -1,2 +1,4 @@
 #Dev Environment Variables
 RG-Name = "Development-RG"
+VN-Name = "Dev-VN"
+VN-CIDR = ["10.0.3.0/24"]
--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "azurerm_resource_group" "AZ-RG" {
   name = var.RG-Name
   location = var.RG-Location
 }
+
+##Creating a Virtual Network
+
+resource "azurerm_virtual_network" "AZ-VN" {
+  name = var.VN-Name
+  resource_group_name = azurerm_resource_group.AZ-RG.name
+  location = azurerm_resource_group.AZ-RG.location
+  address_space = var.VN-CIDR 
+}
